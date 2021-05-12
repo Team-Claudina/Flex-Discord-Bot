45cd0a69e03e95c88ad06b4d642278d388726fb3
GreyStinger <greystinger@gmail.com>
2021-05-12 18:13:41 +0200

    changes

--- cogs/greetings_cog.py
+++ cogs/greetings_cog.py
@@ -2,9 +2,14 @@ from discord.ext import commands
 
 
 class Greeting(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, server="Flex Server"):
         super().__init__()
         self.bot = bot
+        self.server = server
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await member.send('Welcome To {server}'.format(server=self.server))
 
     @commands.command()
     async def hello(self, ctx):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import logging
+import cogs
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -8,13 +9,16 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-prefix = '.'
-bot = commands.Bot(command_prefix=prefix, description='Flex Discord Bot', help_command=None)
+PREFIX = '.'
+SERVER_NAME = 'Flex Server'
+bot = commands.Bot(command_prefix=PREFIX, description='Flex Discord Bot', help_command=None)
+
+bot.add_cog(cogs.greetings_cog.Greeting(bot=bot, server=SERVER_NAME))
 
 
 @bot.event
 async def on_ready():
-    game = discord.Activity(type=discord.ActivityType.watching, name="FlexBot")
+    game = discord.Activity(type=discord.ActivityType.playing, name="FlexBot")
     await bot.change_presence(activity=game, status=discord.Status.idle)
     print(f'On ready triggered and status is set. Logged in with {bot.user}')
 
