10926eb7836c4fda3e808b9e56a49126f1b0e457
GreyStinger <greystinger@gmail.com>
2021-05-20 11:22:37 +0200

    changed env ideas

---
+++ configs/general_config_default.py
@@ -0,0 +1,10 @@
+import discord
+
+embed_colour = discord.Colour.orange()
+
+URL = 'site url'
+IMG_URL = 'img url'
+PREFIX = 'prefix'
+SERVER = 'server name'
+AUTHOR = 'Flex Team'
+SECRET = 'your bot key'
