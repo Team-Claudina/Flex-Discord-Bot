774db8b3bfe47b9c7758e5a628a87f056bf91e0a
GreyStinger <greystinger@gmail.com>
2022-07-29 11:23:29 +0200

    modified configure.bat

--- configure.bat
+++ configure.bat
@@ -2,8 +2,8 @@
 
 if [%1]==[] goto usage
 
-COPY .\configs\embed_config_default.py .\configs\embed_config.py /V
-COPY .\configs\general_config_default.py .\configs\general_config.py /V 
+COPY .\configs\defaults\embed_config_default.py .\configs\embed_config.py /V
+COPY .\configs\defaults\general_config_default.py .\configs\general_config.py /V 
 
 %1\python.exe -m venv .\.venv
 
