91f2ed6de26e9f6159fb597fa7ceb07ced7f94c6
GreyStinger <greystinger@gmail.com>
2022-07-29 17:08:43 +0200

    Made changes to general config

--- configs/defaults/general_config_default.py
+++ configs/defaults/general_config_default.py
@@ -2,11 +2,30 @@ import discord
 
 BOT_NAME = 'Flex Discord Bot'
 URL = 'site url'
-IMG_URL = 'img url'
+IMG_URL = 'https://i.imgur.com/3qfzFxX.png' # Please check supported image file types below. Note: You cannot add one
 PREFIX = 'prefix'
 SERVER = 'server name'
-AUTHOR = 'Flex Team'
 SECRET = 'your bot key'
 PERMISSIONS_INTEGER = "268503104"
 
 embed_colour = discord.Colour.red()
+
+
+# Here for centralization
+# Do not edit
+
+SUPPORTED_IMG_TYPES = [
+	"jpg",
+	"jpeg",
+	"png"
+]
+
+ALL_CONFIG_NAMES = [
+	"BOT_NAME",
+	"URL",
+	"IMG_URL",
+	"PREFIX",
+	"SERVER",
+	"SECRET",
+	"PERMISSION_INTEGER"
+]
