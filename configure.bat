5f9d650c0679340de51c1ed611cb52dae67205ad
GreyStinger <greystinger@gmail.com>
2022-07-29 09:05:44 +0200

    added configure.bat for windows systems

---
+++ configure.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if [%1]==[] goto usage
+
+COPY .\configs\embed_config_default.py .\configs\embed_config.py /V
+COPY .\configs\general_config_default.py .\configs\general_config.py /V 
+
+%1\python.exe -m venv .\.venv
+
+.\.venv\Scripts\python.exe -m pip install --upgrade pip
+.\.venv\Scripts\python.exe -m pip install -r requirements.txt
+
+.\.venv\Scripts\activate.bat
+
+goto :eof
+
+:usage
+@echo Usage: %0 ^<Python Path^>
